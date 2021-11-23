4d5dfb9e4bc1f7cb52ac451922f496b8205c674f
castynet <riungemaina@gmail.com>
2021-11-23 11:59:50 +0300

    frontend/src/components/context: send data to server

--- frontend/src/components/context.jsx
+++ frontend/src/components/context.jsx
@@ -2,25 +2,42 @@ import React, { useContext, useState, useEffect } from "react";
 
 const Context = React.createContext();
 
-export function useApi() {
+export function useApp() {
   return useContext(Context);
 }
 
 export const Provider = ({ children }) => {
-  const [currencies, setCurrencies] = useState([]);
+  const [currencies, setCurrencies] = useState();
+  const [allCurrencies, setAllCurrencies] = useState();
   const [conversions, setConversions] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async function () {
-      var rawData = await fetch("http://localhost:2000/data", {
-        method: "get",
-      });
+      var rawData = await fetch(
+        "https://converter-backend.herokuapp.com/data",
+        {
+          method: "get",
+        }
+      );
       const data = await rawData.json();
-      setCurrencies(data);
+      setCurrencies(data[0]);
+      const allCurrencies = data.slice(0, 5);
+      setAllCurrencies(allCurrencies);
       setLoading(false);
     })();
   }, []);
+
+  async function sendToServer(data) {
+    await fetch("https://converter-backend.herokuapp.com/conversions", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }
 
   return (
     <Context.Provider
@@ -29,6 +46,8 @@ export const Provider = ({ children }) => {
         loading,
         conversions,
         setConversions,
+        sendToServer,
+        allCurrencies,
       }}
     >
       {children}
